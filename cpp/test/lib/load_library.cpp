9112114c8f6c999da9dd95d0f85ffbd894e765ba
Rebecca Pelke <pelke@ice.rwth-aachen.de>
2025-02-13 15:31:29 +0100

    Add exe_mvm test for BNN/TNN interface
    Signed-off-by: Rebecca Pelke <pelke@ice.rwth-aachen.de>

--- cpp/test/lib/load_library.cpp
+++ cpp/test/lib/load_library.cpp
@@ -15,6 +15,28 @@ TEST(LibraryTests, LoadTest) {
     dlclose(handle);
 }
 
+TEST(LibraryTests, MVMTest) {
+    void *handle = dlopen("libBNN_TNN_Interface.so", RTLD_LAZY);
+    ASSERT_NE(handle, nullptr);
+
+    auto exe_mvm_ptr = (int32_t (*)(int32_t *, int32_t *, int32_t *, int32_t, int32_t)) dlsym(handle, "exe_mvm");
+    ASSERT_NE(exe_mvm_ptr, nullptr) << "Failed to load function exe_mvm";
+
+    int32_t res[2] = {0, 0};
+    int32_t vec[2] = {1, -1};
+    int32_t mat[4] = {1, 2, 3, -1};
+    int32_t m_matrix = 2;
+    int32_t n_matrix = 2;
+
+    int32_t status = exe_mvm_ptr(res, vec, mat, m_matrix, n_matrix);
+    ASSERT_EQ(status, 0) << "Matrix-vector multiplication failed";
+
+    EXPECT_EQ(res[0], -1);
+    EXPECT_EQ(res[1], 4);
+
+    dlclose(handle);
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
