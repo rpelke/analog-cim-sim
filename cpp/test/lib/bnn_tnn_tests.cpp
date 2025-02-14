d1e4ebad8d39ee6fffc9f5d80577883086e4f5c3
Rebecca Pelke <pelke@ice.rwth-aachen.de>
2025-02-14 11:17:34 +0100

    Change test infrastructure
    Signed-off-by: Rebecca Pelke <pelke@ice.rwth-aachen.de>

--- cpp/test/lib/bnn_tnn_tests.cpp
+++ cpp/test/lib/bnn_tnn_tests.cpp
@@ -2,26 +2,6 @@
 #include <dlfcn.h>
 #include <cstdlib>
 
-TEST(LibraryTests, LoadTest) {
-    const char* ldLibraryPath = std::getenv("LD_LIBRARY_PATH");
-    
-    if (ldLibraryPath) {
-        std::cout << "LD_LIBRARY_PATH: " << ldLibraryPath << std::endl;
-    } else {
-        std::cout << "LD_LIBRARY_PATH is not set." << std::endl;
-    }
-
-    void *handle_lbnntnn = dlopen("libBNN_TNN_Interface.so", RTLD_LAZY);
-    ASSERT_NE(handle_lbnntnn, nullptr);
-    dlclose(handle_lbnntnn);
-
-    ASSERT_EQ(setenv("CFG_FILE", "configs/config.json", 1), 0) << "Failed to set environment variable";
-
-    void *handle_lint = dlopen("libINT_Interface.so", RTLD_LAZY);
-    ASSERT_NE(handle_lint, nullptr);
-    dlclose(handle_lint);
-}
-
 TEST(LibraryTests, MVMTest) {
     void *handle = dlopen("libBNN_TNN_Interface.so", RTLD_LAZY);
     ASSERT_NE(handle, nullptr);
