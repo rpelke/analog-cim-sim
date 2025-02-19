fb88079019a6b20ca1dbe8c05a518cd89b129108
Rebecca Pelke <pelke@ice.rwth-aachen.de>
2025-02-19 09:49:38 +0100

    Add non-pybind functions in the interface to enable shared lib calls
    Signed-off-by: Rebecca Pelke <pelke@ice.rwth-aachen.de>

--- cpp/src/interface_int.cpp
+++ cpp/src/interface_int.cpp
@@ -27,16 +27,22 @@ extern "C" void set_config(const char *cfg_file) {
         CFG.max_adc_curr, CFG.alpha, CFG.resolution, CFG.verbose);
 }
 
-extern "C" int32_t exe_mvm(pybind11::array_t<int32_t> res,
-                           pybind11::array_t<int32_t> vec,
-                           pybind11::array_t<int32_t> mat, int32_t m_matrix,
-                           int32_t n_matrix) {
+extern "C" int32_t exe_mvm(int32_t *res, int32_t *vec, int32_t *mat,
+                           int32_t m_matrix, int32_t n_matrix) {
     if (xbar == nullptr) {
         std::cerr << "Error: Crossbar is not initialized. Please call "
                      "set_config() first."
                   << std::endl;
         return -1;
     }
+    xbar->mvm(res, vec, mat, m_matrix, n_matrix);
+    return 0;
+}
+
+extern "C" int32_t exe_mvm_pb(pybind11::array_t<int32_t> res,
+                              pybind11::array_t<int32_t> vec,
+                              pybind11::array_t<int32_t> mat, int32_t m_matrix,
+                              int32_t n_matrix) {
     auto res_buffer = res.request();
     auto vec_buffer = vec.request();
     auto mat_buffer = mat.request();
@@ -49,22 +55,26 @@ extern "C" int32_t exe_mvm(pybind11::array_t<int32_t> res,
     return 0;
 }
 
-extern "C" int32_t cpy_mtrx(pybind11::array_t<int32_t> mat, int32_t m_matrix,
-                            int32_t n_matrix) {
+extern "C" int32_t cpy_mtrx(int32_t *mat, int32_t m_matrix, int32_t n_matrix) {
     if (xbar == nullptr) {
         std::cerr << "Error: Crossbar is not initialized. Please call "
                      "set_config() first."
                   << std::endl;
         return -1;
     }
-    auto mat_buffer = mat.request();
-    int32_t *mat_ptr = static_cast<int32_t *>(mat_buffer.ptr);
-    xbar->write(mat_ptr, m_matrix, n_matrix);
+    xbar->write(mat, m_matrix, n_matrix);
     return 0;
 }
 
+extern "C" int32_t cpy_mtrx_pb(pybind11::array_t<int32_t> mat, int32_t m_matrix,
+                               int32_t n_matrix) {
+    auto mat_buffer = mat.request();
+    int32_t *mat_ptr = static_cast<int32_t *>(mat_buffer.ptr);
+    return cpy_mtrx(mat_ptr, m_matrix, n_matrix);
+}
+
 PYBIND11_MODULE(acs_int, m) {
-    m.def("cpy_mtrx", &cpy_mtrx, "Copy matrix to crossbar.");
-    m.def("exe_mvm", &exe_mvm, "Execute matrix-vector multiplication.");
+    m.def("cpy", &cpy_mtrx_pb, "Copy matrix to crossbar.");
+    m.def("mvm", &exe_mvm_pb, "Execute matrix-vector multiplication.");
     m.def("set_config", &set_config, "Set a config for the crossbar.");
 }
--- int-bindings/test/interface_test.py
+++ int-bindings/test/interface_test.py
@@ -1,3 +1,10 @@
+##############################################################################
+# Copyright (C) 2025 Rebecca Pelke                                           #
+# All Rights Reserved                                                        #
+#                                                                            #
+# This is work is licensed under the terms described in the LICENSE file     #
+# found in the root directory of this source tree.                           #
+##############################################################################
 import unittest
 import numpy as np
 import acs_int
@@ -13,8 +20,8 @@ class TestIntInterface(unittest.TestCase):
         res = np.array([1, 1, -1], dtype=np.int32)
 
         acs_int.set_config(os.path.abspath("int-bindings/test/configs/I_DIFF_W_DIFF_1XB.json"))
-        acs_int.cpy_mtrx(mat, m_matrix, n_matrix)
-        acs_int.exe_mvm(res, vec, mat, m_matrix, n_matrix)
+        acs_int.cpy(mat, m_matrix, n_matrix)
+        acs_int.mvm(res, vec, mat, m_matrix, n_matrix)
         np.testing.assert_array_equal(res, np.array([-13759, -119, -1386], dtype=np.int32))
 
     def test_simple_I_DIFF_W_DIFF_2XB(self):
@@ -25,8 +32,8 @@ class TestIntInterface(unittest.TestCase):
         res = np.array([1, 1, -1], dtype=np.int32)
 
         acs_int.set_config(os.path.abspath("int-bindings/test/configs/I_DIFF_W_DIFF_2XB.json"))
-        acs_int.cpy_mtrx(mat, m_matrix, n_matrix)
-        acs_int.exe_mvm(res, vec, mat, m_matrix, n_matrix)
+        acs_int.cpy(mat, m_matrix, n_matrix)
+        acs_int.mvm(res, vec, mat, m_matrix, n_matrix)
         np.testing.assert_array_equal(res, np.array([-13759, -119, -1386], dtype=np.int32))
 
     def test_simple_I_OFFS_W_DIFF(self):
@@ -37,8 +44,8 @@ class TestIntInterface(unittest.TestCase):
         res = np.array([1, 1, -1], dtype=np.int32)
 
         acs_int.set_config(os.path.abspath("int-bindings/test/configs/I_OFFS_W_DIFF.json"))
-        acs_int.cpy_mtrx(mat, m_matrix, n_matrix)
-        acs_int.exe_mvm(res, vec, mat, m_matrix, n_matrix)
+        acs_int.cpy(mat, m_matrix, n_matrix)
+        acs_int.mvm(res, vec, mat, m_matrix, n_matrix)
         np.testing.assert_array_equal(res, np.array([-13759, -119, -1386], dtype=np.int32))
 
     def test_simple_I_TC_W_DIFF(self):
@@ -49,8 +56,8 @@ class TestIntInterface(unittest.TestCase):
         res = np.array([1, 1, -1], dtype=np.int32)
 
         acs_int.set_config(os.path.abspath("int-bindings/test/configs/I_TC_W_DIFF.json"))
-        acs_int.cpy_mtrx(mat, m_matrix, n_matrix)
-        acs_int.exe_mvm(res, vec, mat, m_matrix, n_matrix)
+        acs_int.cpy(mat, m_matrix, n_matrix)
+        acs_int.mvm(res, vec, mat, m_matrix, n_matrix)
         np.testing.assert_array_equal(res, np.array([-13759, -119, -1386], dtype=np.int32))
 
     def test_simple_I_UINT_W_DIFF(self):
@@ -61,8 +68,8 @@ class TestIntInterface(unittest.TestCase):
         res = np.array([1, 1, -1], dtype=np.int32)
 
         acs_int.set_config(os.path.abspath("int-bindings/test/configs/I_UINT_W_DIFF.json"))
-        acs_int.cpy_mtrx(mat, m_matrix, n_matrix)
-        acs_int.exe_mvm(res, vec, mat, m_matrix, n_matrix)
+        acs_int.cpy(mat, m_matrix, n_matrix)
+        acs_int.mvm(res, vec, mat, m_matrix, n_matrix)
         np.testing.assert_array_equal(res, np.array([10241, 121, -1386], dtype=np.int32))
 
     def test_simple_I_UINT_W_OFFS(self):
@@ -73,8 +80,8 @@ class TestIntInterface(unittest.TestCase):
         res = np.array([1, 1, -1], dtype=np.int32)
 
         acs_int.set_config(os.path.abspath("int-bindings/test/configs/I_UINT_W_OFFS.json"))
-        acs_int.cpy_mtrx(mat, m_matrix, n_matrix)
-        acs_int.exe_mvm(res, vec, mat, m_matrix, n_matrix)
+        acs_int.cpy(mat, m_matrix, n_matrix)
+        acs_int.mvm(res, vec, mat, m_matrix, n_matrix)
         np.testing.assert_array_equal(res, np.array([10241, 121, -1386], dtype=np.int32))
 
 
