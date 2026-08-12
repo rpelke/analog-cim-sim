da89415024d6320ce6e90b90b9250a3f7362eb58
Joel Klein <113847502+jmkle@users.noreply.github.com>
2026-08-12 10:37:38 +0200

    Fix shellcheck findings in scripts/build_acs.sh
    Signed-off-by: Joel Klein <113847502+jmkle@users.noreply.github.com>

--- scripts/build_acs.sh
+++ scripts/build_acs.sh
@@ -11,13 +11,13 @@ PYTHON_VERSION=$(python3 -c "import sys; print(f'{sys.version_info.major}.{sys.v
 export PY_PACKAGE_DIR="${PROJECT_ROOT}/.venv/lib/python${PYTHON_VERSION}"
 
 mkdir -p "${PROJECT_ROOT}/build/debug/build"
-cd "${PROJECT_ROOT}/build/debug/build"
+cd "${PROJECT_ROOT}/build/debug/build" || exit 1
 
 # Build the project
 cmake \
     -DCMAKE_BUILD_TYPE=Debug \
-    -DPY_INSTALL_PATH=${PY_PACKAGE_DIR}/site-packages \
-    -DCMAKE_PREFIX_PATH=${PY_PACKAGE_DIR}/pybind11/share/cmake/pybind11 \
+    -DPY_INSTALL_PATH="${PY_PACKAGE_DIR}/site-packages" \
+    -DCMAKE_PREFIX_PATH="${PY_PACKAGE_DIR}/pybind11/share/cmake/pybind11" \
     -DCMAKE_INSTALL_PREFIX=../ \
     -DLIB_TESTS=ON \
     -DCOVERAGE=ON \
