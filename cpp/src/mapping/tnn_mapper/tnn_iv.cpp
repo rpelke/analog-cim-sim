8cdfef166efd0752b9ef5f2982b5b882bb9862f7
Arunkumar Vaidyanathan <vaidyanathan@ice.rwth-aachen.de>
2026-08-18 21:29:58 +0200

    Fix reccuring bug in TNN IV
    Improves upon change in analog correction in a60a430 to be equally
    divided among positive and negative components.
    
    Prior to this change VGG7 accuracy was unusually low (even with
    training set).

--- cpp/src/mapping/tnn_mapper/tnn_iv.cpp
+++ cpp/src/mapping/tnn_mapper/tnn_iv.cpp
@@ -130,7 +130,7 @@ void MapperTnnIV::a_mvm(int32_t *res, const int32_t *vec, const int32_t *mat,
             }
         }
         adc_->convert(tmp_out_, tmp_out_, m_matrix, 1 / i_mm_,
-                      analog_correction / 2, l_name);
+                      analog_correction * 0.25, l_name);
         for (size_t m = 0; m < m_matrix; ++m) {
             tmp_out_fp_[m] += tmp_out_[m];
         }
@@ -143,7 +143,7 @@ void MapperTnnIV::a_mvm(int32_t *res, const int32_t *vec, const int32_t *mat,
             }
         }
         adc_->convert(tmp_out_, tmp_out_, m_matrix, 1 / i_mm_,
-                      -analog_correction / 2, l_name);
+                      -analog_correction * 0.25, l_name);
         for (size_t m = 0; m < m_matrix; ++m) {
             tmp_out_fp_[m] -= tmp_out_[m];
         }
@@ -155,7 +155,8 @@ void MapperTnnIV::a_mvm(int32_t *res, const int32_t *vec, const int32_t *mat,
                 tmp_out_[m] += ia_m_[m][n] * vd_p_[n];
             }
         }
-        adc_->convert(tmp_out_, tmp_out_, m_matrix, 2 / i_mm_, 0.0, l_name);
+        adc_->convert(tmp_out_, tmp_out_, m_matrix, 2 / i_mm_,
+                      -analog_correction * 0.125, l_name);
         for (size_t m = 0; m < m_matrix; ++m) {
             tmp_out_fp_[m] -= tmp_out_[m];
         }
@@ -167,7 +168,8 @@ void MapperTnnIV::a_mvm(int32_t *res, const int32_t *vec, const int32_t *mat,
                 tmp_out_[m] += ia_m_[m][n] * vd_m_[n];
             }
         }
-        adc_->convert(tmp_out_, tmp_out_, m_matrix, 2 / i_mm_, 0.0, l_name);
+        adc_->convert(tmp_out_, tmp_out_, m_matrix, 2 / i_mm_,
+                      analog_correction * 0.125, l_name);
         for (size_t m = 0; m < m_matrix; ++m) {
             tmp_out_fp_[m] += tmp_out_[m];
         }
@@ -180,9 +182,9 @@ void MapperTnnIV::a_mvm(int32_t *res, const int32_t *vec, const int32_t *mat,
             tmp_out_msb_[m] = tmp_out_[2 * m + 1];
         }
         adc_->convert(tmp_out_lsb_, tmp_out_lsb_, m_matrix, 1 / i_mm_,
-                      analog_correction / 2, l_name);
-        adc_->convert(tmp_out_msb_, tmp_out_msb_, m_matrix, 2 / i_mm_, 0.0,
-                      l_name);
+                      analog_correction * 0.25, l_name);
+        adc_->convert(tmp_out_msb_, tmp_out_msb_, m_matrix, 2 / i_mm_,
+                      -analog_correction * 0.125, l_name);
         for (size_t m = 0; m < m_matrix; m++) {
             tmp_out_fp_[m] += tmp_out_lsb_[m];
             tmp_out_fp_[m] -= tmp_out_msb_[m];
@@ -195,9 +197,9 @@ void MapperTnnIV::a_mvm(int32_t *res, const int32_t *vec, const int32_t *mat,
             tmp_out_msb_[m] = tmp_out_[2 * m + 1];
         }
         adc_->convert(tmp_out_lsb_, tmp_out_lsb_, m_matrix, 1 / i_mm_,
-                      -analog_correction / 2, l_name);
-        adc_->convert(tmp_out_msb_, tmp_out_msb_, m_matrix, 2 / i_mm_, 0.0,
-                      l_name);
+                      -analog_correction * 0.25, l_name);
+        adc_->convert(tmp_out_msb_, tmp_out_msb_, m_matrix, 2 / i_mm_,
+                      analog_correction * 0.125, l_name);
         for (size_t m = 0; m < m_matrix; m++) {
             tmp_out_fp_[m] -= tmp_out_lsb_[m];
             tmp_out_fp_[m] += tmp_out_msb_[m];
